d978cc05954ff6dc430c67c1ac8a7f9b2e632183
zqr621 <51789258+Fattyzqrrrrr621@users.noreply.github.com>
2024-07-10 22:03:30 +0800

    zqr test TicketCollection

--- src/main/java/TicketCollection.java
+++ src/main/java/TicketCollection.java
@@ -1,25 +1,53 @@
 import java.util.ArrayList;
 
 public class TicketCollection {
-	
-	public static ArrayList<Ticket> tickets;
+
+	private static ArrayList<Ticket> tickets = new ArrayList<>();
 
 	public static ArrayList<Ticket> getTickets() {
-		return tickets;
+		return new ArrayList<>(tickets);
+	}
+
+	public static void addTicket(Ticket ticket) {
+		validateTicket(ticket);
+		tickets.add(ticket);
 	}
 
 	public static void addTickets(ArrayList<Ticket> tickets_db) {
-		TicketCollection.tickets.addAll(tickets_db);
+		for (Ticket ticket : tickets_db) {
+			validateTicket(ticket);
+		}
+		tickets.addAll(tickets_db);
 	}
-	
-	public static void getAllTickets() {
-    	//display all available tickets from the Ticket collection
-    }
-	public static Ticket getTicketInfo(int ticket_id) {
-    	//SELECT a ticket where ticket id = ticket_id
-    	return null;
 
-    }
-	
+	public static void getAllTickets() {
+		//display all available tickets from the Ticket collection
+		for (Ticket ticket : tickets) {
+			System.out.println(ticket.toString());
+		}
+	}
+	public static Ticket getTicketInfo(int ticket_id) {
+		//SELECT a ticket where ticket id = ticket_id
+		for (Ticket ticket : tickets) {
+			if (ticket.getTicket_id() == ticket_id) {
+				return ticket;
+			}
+		}
+		return null;// or throw an exception if preferred
+
+	}
+
+	private static void validateTicket(Ticket ticket) {
+		if (ticket == null) {
+			throw new IllegalArgumentException("Ticket cannot be null.");
+		}
+
+		// Add more validation logic if necessary
+	}
+
+	public static void clearTickets() {
+		tickets.clear();
+	}
+
 
 }
---
+++ src/test/java/TicketCollectionTest.java
@@ -0,0 +1,328 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketCollectionTest {
+
+    private Airplane airplane;
+    private Flight flight;
+    private Passenger passenger;
+    private Ticket ticket;
+
+    @BeforeEach
+    void setUp() {
+        airplane = new Airplane(1, "Boeing 747",
+                50, 200, 10);
+        flight = new Flight(1, "New York", "Los Angeles", "NY123",
+                "Monash", Timestamp.valueOf("2024-07-01 10:00:00"), Timestamp.valueOf("2024-07-01 12:00:00"), airplane);
+        passenger = new Passenger("John", "Doe", 30, "Man",
+                "john.doe@example.com", "0412345678", "A12345678",
+                "1234567890123456", 123);
+        ticket = new Ticket(1, 1000, flight, false, passenger);
+        TicketCollection.clearTickets(); // 清空静态票据列表
+    }
+
+    @Test
+    @DisplayName("Test the function of adding and getting a ticket")
+    void testAddAndGetATicket() {
+        TicketCollection.addTicket(ticket);
+        ArrayList<Ticket> tickets = TicketCollection.getTickets();
+        assertEquals(1, tickets.size());
+        assertTrue(tickets.contains(ticket));
+    }
+
+    @Test
+    @DisplayName("Test the function of adding and getting tickets")
+    void testAddAndGetTickets() {
+        Airplane airplane1 = new Airplane(2, "Boeing 767",
+                50, 200, 10);
+        Flight flight1 = new Flight(2, "Los Angeles", "New York", "LA123",
+                "Monash", Timestamp.valueOf("2024-07-08 10:00:00"), Timestamp.valueOf("2024-07-08 12:00:00"), airplane1);
+        Passenger passenger1 = new Passenger("Wendy", "Smith", 24, "Woman",
+                "wendy.smith@monash.edu", "0456781234", "A56781234",
+                "1234561234567890", 312);
+        Ticket ticket1 = new Ticket(2, 2000, flight1, false, passenger1);
+
+        ArrayList<Ticket> tickets = new ArrayList<>();
+        tickets.add(ticket);
+        tickets.add(ticket1);
+        TicketCollection.addTickets(tickets);
+
+        assertEquals(2, tickets.size());
+        assertTrue(tickets.contains(ticket1));
+        assertTrue(tickets.contains(ticket));
+    }
+
+    @Test
+    @DisplayName("Test the function of getting all tickets' information")
+    void testGetALLTickets() {
+        Airplane airplane1 = new Airplane(2, "Boeing 767",
+                50, 200, 10);
+        Flight flight1 = new Flight(2, "Los Angeles", "New York", "LA123",
+                "Monash", Timestamp.valueOf("2024-07-08 10:00:00"), Timestamp.valueOf("2024-07-08 12:00:00"), airplane1);
+        Passenger passenger1 = new Passenger("Wendy", "Smith", 24, "Woman",
+                "wendy.smith@monash.edu", "0456781234", "A56781234",
+                "1234561234567890", 312);
+        Ticket ticket1 = new Ticket(2, 2000, flight1, false, passenger1);
+
+        ArrayList<Ticket> tickets = new ArrayList<>();
+        tickets.add(ticket);
+        tickets.add(ticket1);
+        TicketCollection.addTickets(tickets);
+
+        // Redirect system output to test
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        TicketCollection.getAllTickets();
+
+        String test1 = ticket1.toString();
+
+        // 重置系统输出
+        System.setOut(System.out);
+        String expectedOutput = ticket.toString() + System.lineSeparator() +
+                test1 + System.lineSeparator();
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    @DisplayName("Test the function of getting a specific ticket's information")
+    void testGetTicketsInfo() {
+        Airplane airplane1 = new Airplane(2, "Boeing 767",
+                50, 200, 10);
+        Flight flight1 = new Flight(2, "Los Angeles", "New York", "LA123",
+                "Monash", Timestamp.valueOf("2024-07-08 10:00:00"), Timestamp.valueOf("2024-07-08 12:00:00"), airplane1);
+        Passenger passenger1 = new Passenger("Wendy", "Smith", 24, "Woman",
+                "wendy.smith@monash.edu", "0456781234", "A56781234",
+                "1234561234567890", 312);
+        Ticket ticket1 = new Ticket(2, 2000, flight1, false, passenger1);
+
+        ArrayList<Ticket> tickets = new ArrayList<>();
+        tickets.add(ticket);
+        tickets.add(ticket1);
+        TicketCollection.addTickets(tickets);
+
+        // Redirect system output to test
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        TicketCollection.getAllTickets();
+
+        String test1 = TicketCollection.getTicketInfo(2).toString();
+
+        // 重置系统输出
+        System.setOut(System.out);
+        String expectedOutput = TicketCollection.getTicketInfo(1).toString() + System.lineSeparator() +
+                test1 + System.lineSeparator();
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    @DisplayName("Test if a ticket is valid")
+    void testValidateTicket() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(null);
+        });
+        assertEquals("Ticket cannot be null.", exception.getMessage());
+
+        Exception exception1 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(new Ticket(0, 1000, flight, false, passenger));
+        });
+        assertEquals("Ticket id must be greater than 0.", exception1.getMessage());
+
+        Exception exception2 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, -1, flight, false, passenger));
+        });
+        assertEquals("Price must not be negative.", exception2.getMessage());
+
+        Exception exception3 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, 1000, null, false, passenger));
+        });
+        assertEquals("Flight cannot be null.", exception3.getMessage());
+
+        Exception exception4 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, 1000, flight, false, null));
+        });
+        assertEquals("Passenger cannot be null.", exception4.getMessage());
+
+        Exception exception5 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, 1000,
+                    new Flight(1, "New York", "Los Angeles", "NY123", "Monash",
+                            Timestamp.valueOf("2024-07-08 10:00:00"), Timestamp.valueOf("2024-07-08 12:00:00"), null)
+                    , false, passenger));
+        });
+        assertEquals("Airplane cannot be null.", exception5.getMessage());
+
+        //在TicketTest类中验证了flight其他字段的有效性
+        Exception exception6 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, 1000,
+                    new Flight(0, "New York", "Los Angeles", "NY123", "Monash",
+                            Timestamp.valueOf("2024-07-08 10:00:00"), Timestamp.valueOf("2024-07-08 12:00:00"), airplane)
+                    , false, passenger));
+        });
+        assertEquals("Flight ID must be greater than 0.", exception6.getMessage());
+
+        Exception departFromException = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, 1000,
+                    new Flight(1, "New York", "", "NY123", "Monash",
+                            Timestamp.valueOf("2024-07-08 10:00:00"), Timestamp.valueOf("2024-07-08 12:00:00"), airplane)
+                    , false, passenger));
+        });
+        assertEquals("Departure airport cannot be empty.", departFromException.getMessage());
+
+        Exception departToException = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, 1000,
+                    new Flight(1, "", "Los Angeles", "NY123", "Monash",
+                            Timestamp.valueOf("2024-07-08 10:00:00"), Timestamp.valueOf("2024-07-08 12:00:00"), airplane)
+                    , false, passenger));
+        });
+        assertEquals("Destination airport cannot be empty.", departToException.getMessage());
+
+        Exception exception7 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, 1000,
+                    new Flight(1, "New York", "Los Angeles", "", "Monash",
+                            Timestamp.valueOf("2024-07-08 10:00:00"), Timestamp.valueOf("2024-07-08 12:00:00"), airplane)
+                    , false, passenger));
+        });
+        assertEquals("Flight code cannot be empty.", exception7.getMessage());
+
+        Exception exception8 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, 1000,
+                    new Flight(1, "New York", "Los Angeles", "NY123", "",
+                            Timestamp.valueOf("2024-07-08 10:00:00"), Timestamp.valueOf("2024-07-08 12:00:00"), airplane)
+                    , false, passenger));
+        });
+        assertEquals("Airline company cannot be empty.", exception8.getMessage());
+
+        assertThrows(NullPointerException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, 1000,
+                    new Flight(1, "New York", "Los Angeles", "NY123", "Monash",
+                            null, Timestamp.valueOf("2024-07-08 12:00:00"), airplane)
+                    , false, passenger));
+        });
+
+        assertThrows(NullPointerException.class, () -> {
+            TicketCollection.addTicket(new Ticket(1, 1000,
+                    new Flight(1, "New York", "Los Angeles", "NY123", "Monash",
+                            Timestamp.valueOf("2024-07-08 10:00:00"), null, airplane)
+                    , false, passenger));
+        });
+
+        IllegalArgumentException exception9 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(
+                    new Ticket(1, 1000, flight, false,
+                            new Passenger("John3", "Doe", 30,
+                            "Man", "john.doe@example.com", "0456789123",
+                            "A12345678", "1234567890123456", 123)));
+        });
+        assertEquals("Invalid first name: John3", exception9.getMessage());
+
+        IllegalArgumentException exception10 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(
+                    new Ticket(1, 1000, flight, false,
+                            new Passenger("John", "Doe2", 30,
+                            "Man", "john.doe@example.com", "0456789123",
+                            "A12345678", "1234567890123456", 123)));
+        });
+        assertEquals("Invalid second name: Doe2", exception10.getMessage());
+
+        IllegalArgumentException exception11 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(
+                    new Ticket(1, 1000, flight, false,
+                            new Passenger("John", "Doe", -1,
+                            "Man", "john.doe@example.com", "0456789123",
+                            "A12345678", "1234567890123456", 123)));
+        });
+        assertEquals("Age cannot be negative", exception11.getMessage());
+
+        IllegalArgumentException exception12 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(
+                    new Ticket(1, 1000, flight, false,
+                            new Passenger("John", "Doe", 30,
+                            "Male", "john.doe@example.com", "0456789123",
+                            "A12345678", "1234567890123456", 123)));
+        });
+        assertEquals("Invalid gender: Male", exception12.getMessage());
+
+        //在TicketTest类中验证了passenger其他字段的有效性
+        IllegalArgumentException exception13 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(
+                    new Ticket(1, 1000, flight, false,
+                            new Passenger("John", "Doe", 30,
+                            "Man", "invalid-email", "0456789123",
+                            "A12345678", "1234567890123456", 123)));
+        });
+        assertEquals("Invalid email format.", exception13.getMessage());
+
+        IllegalArgumentException exception14 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(
+                    new Ticket(1, 1000, flight, false,
+                            new Passenger("John", "Doe", 30,
+                            "Man", "john.doe@example.com", "invalid-phone-number",
+                            "A12345678", "1234567890123456", 123)));
+        });
+        assertTrue(exception14.getMessage().contains("Invalid phone number format."));
+
+        IllegalArgumentException exception15 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(
+                    new Ticket(1, 1000, flight, false,
+                            new Passenger("John", "Doe", 30,
+                            "Man", "john.doe@example.com", "0456789123",
+                            "A1234567890", "1234567890123456", 123)));
+        });
+        assertTrue(exception15.getMessage().contains("Passport number should not be more than 9 characters long."));
+
+
+        IllegalArgumentException exception16 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(
+                    new Ticket(1, 1000, flight, false,
+                            new Passenger("John", "Doe", 30,
+                            "Man", "john.doe@example.com", "0456789123",
+                            "A123456789", "", 123)));
+        });
+        assertEquals("All fields are required and must be valid.", exception16.getMessage());
+
+        IllegalArgumentException exception17 = assertThrows(IllegalArgumentException.class, () -> {
+            TicketCollection.addTicket(
+                    new Ticket(1, 1000, flight, false,
+                            new Passenger("John", "Doe", 30,
+                            "Man", "john.doe@example.com", "0456789123",
+                            "A12345678", "1234567890123456", -1)));
+        });
+        assertEquals("All fields are required and must be valid.", exception17.getMessage());
+
+    }
+
+    @Test
+    @DisplayName("Test the function of clearing tickets")
+    void testClearTickets() {
+        TicketCollection.addTicket(ticket);
+        TicketCollection.clearTickets();
+        assertEquals(0, TicketCollection.getTickets().size());
+
+        Airplane airplane1 = new Airplane(2, "Boeing 767",
+                50, 200, 10);
+        Flight flight1 = new Flight(2, "Los Angeles", "New York", "LA123",
+                "Monash", Timestamp.valueOf("2024-07-08 10:00:00"), Timestamp.valueOf("2024-07-08 12:00:00"), airplane1);
+        Passenger passenger1 = new Passenger("Wendy", "Smith", 24, "Woman",
+                "wendy.smith@monash.edu", "0456781234", "A56781234",
+                "1234561234567890", 312);
+        Ticket ticket1 = new Ticket(2, 2000, flight1, false, passenger1);
+
+        ArrayList<Ticket> tickets = new ArrayList<>();
+        tickets.add(ticket);
+        tickets.add(ticket1);
+        TicketCollection.addTickets(tickets);
+        TicketCollection.clearTickets();
+        assertEquals(0, TicketCollection.getTickets().size());
+    }
+
+
+}
